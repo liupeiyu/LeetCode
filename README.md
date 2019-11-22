180c9914af7e3c59697edcd79c5976d8b3b5c418
liupeiyu <lvegod@163.com>
2019-11-22 18:02:43 +0800

    String

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # LeetCode
+# git add README.md
+# git commit -m "first commit"
+# git remote add origin git@github.com:liupeiyu/LeetCode.git
+# git push -u origin master
---
+++ String/string_804.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-11-22 17:56
+# @Author  : peiyu
+# @Email   : lvegod@163.com
+# @File    : string_804.py
+# @Software: PyCharm
+
+'''
+
+
+
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation:
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+'''
+
+
+#method
+
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        dicts={'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..'}
+
+        li=[]
+        for word in words:
+            tmp = ''
+            for i in word:
+                tmp=tmp+dicts[i]
+
+            li.append(tmp)
+
+        return set(li).__len__()
