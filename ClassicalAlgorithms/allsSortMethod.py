56a6c8211c24a098aafbe60ea54ba2b08c253918
liupeiyu <lvegod@163.com>
2021-07-14 14:10:11 +0800

    finish binar_sort_search method

--- ClassicalAlgorithms/allsSortMethod.py
+++ ClassicalAlgorithms/allsSortMethod.py
@@ -8,5 +8,15 @@
 def select_sort_search():
     return
 
-def binary_sort_search():
-    return
+def binary_sort_search(li:list,val:int):
+    left=0
+    right=len(li)-1
+    mid  = (left+right)//2
+    while left<=right:
+        if li[mid]==val:
+            return mid
+        elif li[mid]<val:
+            left =mid+1
+        else:
+            right=mid-1
+    return None
